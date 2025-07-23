37cdf0be7f988d7b214ed1a45da2bfce4aedbc21
Emily Harbeck <eharbeck@me.com>
2025-07-23 17:34:10 +1000

    completed lab and passed all tests

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
+function add() {
+
+}
+function subtract() {
+
+}
+function multiply() {
+
+}
+function divide() {
+
+}
 function add(a, b) {
     return a + b;
 }
 function subtract(a, b) {
     return a - b;
 }
-function multiply(a, b) { 
+function multiply(a , b) {
     return a * b;
 }
 function divide(a, b) {
@@ -17,8 +29,8 @@ function decrement(n) {
     return (n -= 1);
 }
 function makeInt(n) {
-    return parseInt(n ,10)  
+   return parseInt(n, 10);
 }
 function preserveDecimal(n) {
-    return parseFloat(n);    
+    return parseFloat(n);
 }
